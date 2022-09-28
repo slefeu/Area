600a80c56750d68749f8ba1c179856eaa6573119
arthurtakase <arthur.decaen@epitech.eu>
2022-09-28 22:29:49 +0200

    FEAT: cordova android build

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { Component }  from 'react';
 
 function App() {
   return (
--- src/index.jsx
+++ src/index.jsx
@@ -4,12 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const startApp = () => {
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
+
+if (window.cordova) {
+  document.addEventListener('deviceready', startApp, false);
+} else {
+  startApp()
+}
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
