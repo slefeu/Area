77b9598299d683f5c4d4c3a6ac1e9b9f70054192
Solène Lefeu <solene.lefeu@epitech.eu>
2022-10-03 16:25:42 +0200

    ADD: base files and routes to create the login page

---
+++ front/src/Authentification/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Login.css'
+
+function Login() {
+    return (
+        <div className='LoginTitle'>
+            Ceci est un titre
+        </div>
+    );
+}
+
+export default Login;
