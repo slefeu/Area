3d089d9ae2c48688637a78ef040b8704275635f5
arthurtakase <arthur.decaen@epitech.eu>
2022-10-04 12:03:50 +0200

    FEAT: remove useless front files

--- front/src/index.jsx
+++ front/src/index.jsx
@@ -1,16 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './css/index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const startApp = () => {
   const root = ReactDOM.createRoot(document.getElementById('root'));
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
+  root.render(<App />);
 }
 
 if (window.cordova) {
@@ -18,9 +13,3 @@ if (window.cordova) {
 } else {
   startApp()
 }
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
