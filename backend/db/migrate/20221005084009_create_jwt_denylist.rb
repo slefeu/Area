11545d0511c7ec63adf5c4d1e350854b228262bf
Glab <geoffrey.labruyere@epitech.eu>
2022-10-09 22:21:37 +0200

    [ADD][BACKEND] force signout

--- backend/db/migrate/20221005084009_create_jwt_denylist.rb
+++ backend/db/migrate/20221005084009_create_jwt_denylist.rb
@@ -1,4 +1,4 @@
-class CreateJwtDenylist < ActiveRecord::Migration[6.1]
+class CreateJwtDenylist < ActiveRecord::Migration[7.0]
   def change
     create_table :jwt_denylist do |t|
       t.string :jti, null: false
