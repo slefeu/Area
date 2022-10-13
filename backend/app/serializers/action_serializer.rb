1b6ff6090899c85f3ee0a79905c7ddef2d745c46
roy-corentin <corentin.roy@epitech.eu>
2022-10-13 14:28:15 +0200

    [ADD] add annotate on serializer

--- backend/app/serializers/action_serializer.rb
+++ backend/app/serializers/action_serializer.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: actions
+#
+#  id         :bigint           not null, primary key
+#  klass      :string           not null
+#  options    :jsonb            not null
+#  widget_id  :bigint
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class ActionSerializer < ActiveModel::Serializer
   attributes :id, :name, :options
 
