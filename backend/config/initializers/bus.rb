163b89d480787ac8b0fa4e2b82abc26b7f676d1d
Corentin Roy <croy@mesvaccins.net>
2022-10-14 16:10:11 +0200

    [ADD] add verification for sidekiq var

--- backend/config/initializers/bus.rb
+++ backend/config/initializers/bus.rb
@@ -4,9 +4,9 @@ require_relative "router_actions"
 require_relative "router_reactions"
 
 Rails.application.reloader.to_prepare do
-  Widget_to_kill = WidgetToKill.new
-  Widget_to_disable = WidgetToDisable.new
+  Widget_to_kill ||= WidgetToKill.new
+  Widget_to_disable ||= WidgetToDisable.new
 
-  Bus_actions = BusActions.new(RouterAction)
-  Bus_reactions = BusReactions.new(RouterReaction)
+  Bus_actions ||= BusActions.new(RouterAction)
+  Bus_reactions ||= BusReactions.new(RouterReaction)
 end
