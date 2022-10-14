0699c784a671c8796e10f3033a6553818ea60ce2
Corentin Roy <croy@mesvaccins.net>
2022-10-14 17:26:16 +0200

    [FIX] fix spec due to the route's update

--- backend/spec/routing/actions_routing_spec.rb
+++ backend/spec/routing/actions_routing_spec.rb
@@ -9,22 +9,5 @@ RSpec.describe ActionsController, type: :routing do
     it "routes to #show" do
       expect(get: "/actions/1").to route_to("actions#show", id: "1")
     end
-
-
-    it "routes to #create" do
-      expect(post: "/actions").to route_to("actions#create")
-    end
-
-    it "routes to #update via PUT" do
-      expect(put: "/actions/1").to route_to("actions#update", id: "1")
-    end
-
-    it "routes to #update via PATCH" do
-      expect(patch: "/actions/1").to route_to("actions#update", id: "1")
-    end
-
-    it "routes to #destroy" do
-      expect(delete: "/actions/1").to route_to("actions#destroy", id: "1")
-    end
   end
 end
--- backend/spec/routing/reactions_routing_spec.rb
+++ backend/spec/routing/reactions_routing_spec.rb
@@ -9,22 +9,5 @@ RSpec.describe ReactionsController, type: :routing do
     it "routes to #show" do
       expect(get: "/reactions/1").to route_to("reactions#show", id: "1")
     end
-
-
-    it "routes to #create" do
-      expect(post: "/reactions").to route_to("reactions#create")
-    end
-
-    it "routes to #update via PUT" do
-      expect(put: "/reactions/1").to route_to("reactions#update", id: "1")
-    end
-
-    it "routes to #update via PATCH" do
-      expect(patch: "/reactions/1").to route_to("reactions#update", id: "1")
-    end
-
-    it "routes to #destroy" do
-      expect(delete: "/reactions/1").to route_to("reactions#destroy", id: "1")
-    end
   end
 end
