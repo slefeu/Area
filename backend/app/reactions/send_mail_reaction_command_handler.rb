ca6e71acd06bf0a0e7d560721e73b6e9f3703695
Théau Grasseau <theaugrs@MacBook-Air-de-Theau.local>
2022-10-17 13:25:17 +0200

    [ADD] gmail handler

--- backend/app/reactions/send_mail_reaction_command_handler.rb
+++ backend/app/reactions/send_mail_reaction_command_handler.rb
@@ -1,11 +1,21 @@
 # frozen_string_literal: true
+require "Gmail"
 
-class SendMailReactionCommandHandler
+class SendMailCommandHandler
   def initialize
+    Gmail.client_id = ENV["GMAIL_CLIENT_ID"]
+    Gmail.client_secret = ENV["GMAIL_CLIENT_SECRET"]
   end
 
-  def call(attributes)
-    # TODO do something with options
+  def call(attributs)
     puts "Send Mail Command Handler"
+    Gmail.refresh_token = attributs[:token_refreshed]
+    mail = Gmail::Message.new(
+      from: User.find(attributs[:user_id]).email,
+      to: attributs[:to],
+      subject: attributs[:subject],
+      body: attributs[:body]
+    )
+    mail.deliver
   end
 end
