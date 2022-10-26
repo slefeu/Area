cd49c283d2eab3baf45707dba4803958348adbff
Théau Grasseau <theaugrs@MacBook-Air-de-Theau.local>
2022-10-26 11:38:28 +0200

    [ADD] each hour command and its spec

--- backend/app/actions/each_hour_action_command_handler.rb
+++ backend/app/actions/each_hour_action_command_handler.rb
@@ -1,19 +1,19 @@
-class EachMonthActionCommandHandler
+class EachHourActionCommandHandler
     def initialize
     end
   
     def call(attributes, mocked_response = nil)
-      puts "Each Month Command Handler"
+      puts "Each Hour Command Handler"
   
       time_info = mocked_response || HTTParty.get("https://api.timezonedb.com/v2.1/get-time-zone?key=MLW9WKV7JEUS&format=json&by=position&lat=44.8404&lng=-0.5805")
       today = time_info["formatted"].to_time
-      last_month = attributes[:last_month].to_time
-      result = today.day > last_month.day
+      last_hour = attributes[:last_hour].to_time
+      result = today.hour > last_hour.hour
   
       if result
         action = Action.find(attributes[:action_id])
         today = today.to_date
-        action.options["last_month"] = today.to_s
+        action.options["last_hour"] = today.to_s
         action.save
       end
       result
