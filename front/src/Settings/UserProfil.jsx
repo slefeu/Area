1df1b5314e0b3059361628979791f6f0cc556bb3
Solène Lefeu <solene.lefeu@epitech.eu>
2022-10-30 18:30:17 +0100

    [ADD] General information page for the user to see its account infos

---
+++ front/src/Settings/UserProfil.jsx
@@ -0,0 +1,59 @@
+import { useState, useEffect } from "react"
+import { Navigate } from 'react-router-dom';
+
+import Container from '../Tools/Container'
+import AXIOS from "../Tools/Client"
+import Load from "../Tools/Load"
+import Error from "../Tools/Notif"
+import SettingsNavBar from "./SettingsNavBar"
+
+import '../css/settings.css'
+
+function DisplayUserInfos({ data }) {
+
+    return (
+        <Container>
+            <div className="pageTitle">
+                {data.first_name + "'s general information"}
+            </div>
+            <div className="column border">
+                <div>First name</div>
+                <input type="text" readOnly="readonly" className="disabledInput" placeholder={data.first_name} />
+            </div>
+            <div className="column border">
+                <div>Last name</div>
+                <input type="text" readOnly="readonly" className="disabledInput" placeholder={data.last_name} />
+            </div>
+            <div className="column border">
+                <div>Email address</div>
+                <input type="text" readOnly="readonly" className="disabledInput" placeholder={data.email} />
+            </div>
+        </Container>
+    );
+}
+
+function UserProfil() {
+
+    const [element, setElement] = useState(<Load />)
+
+    useEffect(() => {
+        if (!localStorage.getItem('token')) { setElement(<Navigate to="/login" />) }
+
+        const token = "Bearer " + localStorage.getItem("token");
+
+        AXIOS.get(localStorage.getItem("url") + "/current_user", { headers: { Authorization: token } })
+            .then(function (res) { setElement(<DisplayUserInfos data={res.data} />) })
+            .catch(function (err) { setElement(<Error msg="Erreur, " error={err} />) });
+    }, []);
+
+    return (
+        <>
+            <SettingsNavBar currentPage="UserProfil" />
+            <div className="content">
+                {element}
+            </div>
+        </>
+    );
+}
+
+export default UserProfil
