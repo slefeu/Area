40987ac010ceee562f4ddf74889d95cfeec1c683
roy-corentin <corentin.roy@epitech.eu>
2022-10-31 13:58:35 +0100

    [ADD] add route to get current_user's reset_token

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  before_action :user_logged?, only: [ :show_current_user ]
+  before_action :user_logged?, only: %i[ show_current_user reset_token ]
   before_action :set_user, only: %i[ show update destroy ]
   before_action :is_admin?, only: [:destroy]
 
@@ -31,6 +31,17 @@ class UsersController < ApplicationController
   # DELETE /users/1
   def destroy
     @user.destroy
+  end
+
+  # GET /users/reset_token
+  def reset_token
+    raw, hashed = Devise.token_generator.generate(User, :reset_password_token)
+    user = current_user
+    user.reset_password_token = Devise.token_generator.digest(User, :reset_password_token, hashed)
+    user.reset_password_sent_at = Time.now
+    user.save
+
+    render json: hashed
   end
 
   private
