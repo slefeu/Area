a40ba2a92e4a1ef4fbaf33c9dd2a137d4f591aa4
roy-corentin <corentin.roy@epitech.eu>
2022-11-01 20:21:14 +0100

    [UP] update gem for gmail

--- backend/app/reactions/send_mail_reaction_command_handler.rb
+++ backend/app/reactions/send_mail_reaction_command_handler.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
+require "google-apis-gmail_v1"
+
 class SendMailReactionCommandHandler
   def initialize
+    @client = Google::Apis::GmailV1Service.new
   end
 
   def call(attributs)
