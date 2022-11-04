d4cb6a6f671ca7aaf62c9a23489a500f273b42b0
Corentin Roy <croy@mesvaccins.net>
2022-11-04 19:35:44 +0100

    [ADD] add missing actions in bus

--- backend/config/initializers/router_actions.rb
+++ backend/config/initializers/router_actions.rb
@@ -2,6 +2,8 @@
 
 Rails.application.reloader.to_prepare do
   RouterAction ||= Router.new
-  RouterAction.resolve(command: EachDayActionCommand, handler: EachDayActionCommandHandler.new)
   RouterAction.resolve(command: AtHourActionCommand, handler: AtHourActionCommandHandler.new)
+  RouterAction.resolve(command: EachDayActionCommand, handler: EachDayActionCommandHandler.new)
+  RouterAction.resolve(command: EachHourActionCommand, handler: EachHourActionCommandHandler.new)
+  RouterAction.resolve(command: EachMonthActionCommand, handler: EachMonthActionCommandHandler.new)
 end
