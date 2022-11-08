d42547f646fc2e6d40d234d3ac6a87483154e9a2
roy-corentin <corentin.roy@epitech.eu>
2022-11-09 00:25:08 +0100

    [UP] update send mail command handler

--- backend/app/reactions/send_mail_reaction_command_handler.rb
+++ backend/app/reactions/send_mail_reaction_command_handler.rb
@@ -9,8 +9,7 @@ class SendMailReactionCommandHandler
 
   def call(attributs)
     puts "Send Mail Command Handler"
-    user = User.find(attributs[:user_id])
-    gmail = GmailClient.new(user.google_token, user.email)
+    gmail = GmailClient.new(attributs[:token], attributs[:email])
     gmail.send_mail(attributs[:to], attributs[:subject], attributs[:body])
   end
 end
