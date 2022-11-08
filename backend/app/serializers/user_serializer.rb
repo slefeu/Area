fdabd786c8e9b3ea410fef64ef0ba502c1a1b734
roy-corentin <corentin.roy@epitech.eu>
2022-11-08 17:43:49 +0100

    [DEL] delete spotify token from serializer

--- backend/app/serializers/user_serializer.rb
+++ backend/app/serializers/user_serializer.rb
@@ -42,7 +42,7 @@
 #  index_users_on_reset_password_token  (reset_password_token) UNIQUE
 #
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :email, :admin, :background, :spotify_token, :songs, :picture
+  attributes :id, :first_name, :last_name, :email, :admin, :background, :songs, :picture
 
   has_many :widgets
 end
